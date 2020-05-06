a1a47a900267ada8eb40414aae6f5224bcfab240
Javier Sintes <javier@secret-source.eu>
2020-05-06 20:47:06 +0100

    Add transition in "Añadir" button

--- src/app/frutas/frutas.component.ts
+++ src/app/frutas/frutas.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faMapMarkerAlt, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-frutas',
@@ -8,10 +8,27 @@ import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 })
 export class FrutasComponent implements OnInit {
   faMapMarkerAlt = faMapMarkerAlt;
+  faMinus = faMinus;
+  faPlus = faPlus;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onClick(){
+    document.getElementById('button').classList.add('clicked');
+  }
+  increase(){
+    const currentValue = parseInt((document.getElementById('input') as HTMLInputElement).value, 10);
+    (document.getElementById('input') as HTMLInputElement).value = '' + (currentValue + 1);
+  }
+  decrease(){
+    const currentValue = parseInt((document.getElementById('input') as HTMLInputElement).value, 10);
+    if (currentValue === 1){
+      document.getElementById('button').classList.remove('clicked');
+    } else {
+      (document.getElementById('input')  as HTMLInputElement).value = '' + (currentValue - 1) ;
+    }
   }
 
 }
